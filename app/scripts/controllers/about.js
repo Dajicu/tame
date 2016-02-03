e7409c09f2e42be05f806850a76a05537669c26c
Dajicu <davidjimenezcuesta@gmail.com>
2016-02-03 16:26:24 +0100

    web examen inicio

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tameApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tameApp
+ */
+angular.module('tameApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tameApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tameApp
+ */
+angular.module('tameApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
